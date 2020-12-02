3ed953c87b6ca370482513f976640b6aaaf1ce34
Chad Johnson <chad@chadjohn.com>
2020-12-02 14:54:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
+  elsif int % 5 == 0
+    "Buzz"
+  else int % 15 == 0
+    "FizzBuzz"
 end
 
 fizzbuzz(3)
 fizzbuzz(5)
+fizzbuzz(15)
 fizzbuzz()
